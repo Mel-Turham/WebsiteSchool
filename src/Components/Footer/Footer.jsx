009590c6ea960b1df9bff1851f6b88677c776331
Mel/ <tchoikuemel06@gmail.com>
2024-03-15 22:16:38 +0100

    Update hero page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import './Footer.css'
 const Footer = () => {
+
+  const getYear = new Date().getFullYear();
   return (
     <footer className='footer'>
-      <p>&copy; 2024 Edusity. All rights reserved</p>
+      <p>&copy; {getYear} Edusity. All rights reserved</p>
       <ul>
         <li>Terms of services</li>
         <li>Privacy Policy</li>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -19,24 +19,51 @@ const Navbar = () => {
   }
 
   return (
-    <nav className={`container ${sticky ? 'dark-nav' : ""}`}>
-      <img src={Logo} alt="Logo image" className='logo' />
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+      <img src={Logo} alt='Logo image' className='logo' />
       <ul className={`${activeMenu ? '' : 'mobile-menu'}`}>
         <li>
-          <Link to='hero' smooth={true} offset={0}
-            duration={500}>Home</Link>
+          <Link to='hero' smooth={true} offset={0} duration={500}>
+            Home
+          </Link>
         </li>
-        <li> <Link to='program' smooth={true} offset={-260}
-          duration={500}>Program</Link></li>
-        <li> <Link to='about' smooth={true} offset={-260}
-          duration={500}>About us</Link></li>
-        <li> <Link to='campus' smooth={true} offset={-260}
-          duration={500}>Campus</Link></li>
-        <li> <Link to='testimonials' smooth={true} offset={-260}
-          duration={500}>Testimonials</Link></li>
-        <li><Link to='contact' smooth={true} duration={500} offset={-260} className='btn'>Contact us</Link></li>
+        <li>
+          <Link to='program' smooth={true} offset={-260} duration={500}>
+            Program
+          </Link>
+        </li>
+        <li>
+          <Link to='about' smooth={true} offset={-150} duration={500}>
+            About us
+          </Link>
+        </li>
+        <li>
+          <Link to='campus' smooth={true} offset={-260} duration={500}>
+            Campus
+          </Link>
+        </li>
+        <li>
+          <Link to='testimonials' smooth={true} offset={-260} duration={500}>
+            Testimonials
+          </Link>
+        </li>
+        <li>
+          <Link
+            to='contact'
+            smooth={true}
+            duration={500}
+            offset={-260}
+            className='btn'
+          >
+            Contact us
+          </Link>
+        </li>
       </ul>
-      <AiOutlineMenu onClick={handleOnclick} className='toggleButton' role='button' />
+      <AiOutlineMenu
+        onClick={handleOnclick}
+        className='toggleButton'
+        role='button'
+      />
     </nav>
   );
 };
